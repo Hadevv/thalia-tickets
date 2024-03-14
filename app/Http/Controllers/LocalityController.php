3a98372aac92c0f854c9477830c4fe5fd47e18d8
Hadevv <ademeure29@gmail.com>
2024-03-14 16:18:25 +0100

    chore: Models, controllers, templates, routes for Type, Locality, Location, Role

---
+++ app/Http/Controllers/LocalityController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Locality;
+use App\Http\Requests\StoreLocalityRequest;
+use App\Http\Requests\UpdateLocalityRequest;
+
+class LocalityController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreLocalityRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Locality $locality)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Locality $locality)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateLocalityRequest $request, Locality $locality)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Locality $locality)
+    {
+        //
+    }
+}
---
+++ database/seeders/ArtistSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use App\Models\Artist;
+
+class ArtistSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+
+    public function run(): void
+    {
+        Artist::truncate();
+
+        $artists = [
+            ['firstname' => 'Daniel', 'lastname' => 'Marcelin'],
+            ['firstname' => 'Philippe', 'lastname' => 'Laurent'],
+            ['firstname' => 'Marius', 'lastname' => 'Von Mayenburg'],
+            ['firstname' => 'Olivier', 'lastname' => 'Boudon'],
+            ['firstname' => 'Anne Marie', 'lastname' => 'Loop'],
+            ['firstname' => 'Pietro', 'lastname' => 'Varasso'],
+            ['firstname' => 'Laurent', 'lastname' => 'Caron'],
+            ['firstname' => 'Élena', 'lastname' => 'Perez'],
+            ['firstname' => 'Guillaume', 'lastname' => 'Alexandre'],
+            ['firstname' => 'Claude', 'lastname' => 'Semal'],
+            ['firstname' => 'Laurence', 'lastname' => 'Warin'],
+        ];
+
+        DB::table('artists')->insert($artists);
+    }
+}
