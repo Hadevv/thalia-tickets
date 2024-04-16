1c626bb3d4d539ec80c7ec57b5bf54d63cdf46e9
Hadevv <ademeure29@gmail.com>
2024-04-16 16:23:16 +0200

    Change home page and updated the navigation

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -12,68 +12,85 @@
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
-                    </x-nav-link>
-                    {{-- ajout d'artist dans la navigation --}}
-                    <x-nav-link :href="route('artist.index')" :active="request()->routeIs('artist.index')">
-                        {{ __('Artist') }}
-                    </x-nav-link>
-                    {{-- ajout de type dans la navigation --}}
-                    <x-nav-link :href="route('type.index')" :active="request()->routeIs('type.index')">
-                        {{ __('Type') }}
-                    </x-nav-link>
-                    <x-nav-link :href="route('locality.index')" :active="request()->routeIs('locality.index')">
-                        {{ __('Locality') }}
-                    </x-nav-link>
-                    <x-nav-link :href="route('representation.index')" :active="request()->routeIs('representation.index')">
-                        {{ __('Representation') }}
-                    </x-nav-link>
                     <x-nav-link :href="route('show.index')" :active="request()->routeIs('show.index')">
                         {{ __('Spectacle') }}
                     </x-nav-link>
+                    {{-- Guest Links --}}
+                    @guest
+                        <x-nav-link :href="route('login')" :active="request()->routeIs('login')">
+                            {{ __('Login') }}
+                        </x-nav-link>
+                        @if (Route::has('register'))
+                            <x-nav-link :href="route('register')" :active="request()->routeIs('register')">
+                                {{ __('Register') }}
+                            </x-nav-link>
+                        @endif
+                    @endguest
+
+                    {{-- Auth Links --}}
+                    @auth
+                        {{-- ajout d'artist dans la navigation --}}
+                        <x-nav-link :href="route('artist.index')" :active="request()->routeIs('artist.index')">
+                            {{ __('Artist') }}
+                        </x-nav-link>
+                        {{-- ajout de type dans la navigation --}}
+                        <x-nav-link :href="route('type.index')" :active="request()->routeIs('type.index')">
+                            {{ __('Type') }}
+                        </x-nav-link>
+                        <x-nav-link :href="route('locality.index')" :active="request()->routeIs('locality.index')">
+                            {{ __('Locality') }}
+                        </x-nav-link>
+                        <x-nav-link :href="route('representation.index')" :active="request()->routeIs('representation.index')">
+                            {{ __('Representation') }}
+                        </x-nav-link>
+                        <x-nav-link :href="route('show.index')" :active="request()->routeIs('show.index')">
+                            {{ __('Spectacle') }}
+                        </x-nav-link>
+                        <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+                            {{ __('Dashboard') }}
+                        </x-nav-link>
+                    @endauth
                 </div>
             </div>
 
             <!-- Settings Dropdown -->
             <div class="hidden sm:flex sm:items-center sm:ms-6">
-                <x-dropdown align="right" width="48">
-                    <x-slot name="trigger">
-                        <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
-                            <div>{{ Auth::user()->login }}</div>
+                @auth
+                    <x-dropdown align="right" width="48">
+                        <x-slot name="trigger">
+                            <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
+                                <div>{{ Auth::user()->login }}</div>
+                                <div class="ms-1">
+                                    <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                        <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                                    </svg>
+                                </div>
+                            </button>
+                        </x-slot>
 
-                            <div class="ms-1">
-                                <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                                    <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
-                                </svg>
-                            </div>
-                        </button>
-                    </x-slot>
-
-                    <x-slot name="content">
-                        <x-dropdown-link :href="route('profile.edit')">
-                            {{ __('Profile') }}
-                        </x-dropdown-link>
-
-                            <x-dropdown-link :href="route('admin.index')">
-                                {{ __('Admin') }}
+                        <x-slot name="content">
+                            <x-dropdown-link :href="route('profile.edit')">
+                                {{ __('Profile') }}
                             </x-dropdown-link>
+                            @if (Auth::user())
+                                <x-dropdown-link :href="route('admin.index')">
+                                    {{ __('Admin') }}
+                                </x-dropdown-link>
+                            @endif
+                            <!-- Authentication -->
+                            <form method="POST" action="{{ route('logout') }}">
+                                @csrf
 
-
-                        <!-- Authentication -->
-                        <form method="POST" action="{{ route('logout') }}">
-                            @csrf
-
-                            <x-dropdown-link :href="route('logout')"
-                                    onclick="event.preventDefault();
-                                                this.closest('form').submit();">
-                                {{ __('Log Out') }}
-                            </x-dropdown-link>
-                        </form>
-                    </x-slot>
-                </x-dropdown>
+                                <x-dropdown-link :href="route('logout')"
+                                        onclick="event.preventDefault();
+                                                    this.closest('form').submit();">
+                                    {{ __('Log Out') }}
+                                </x-dropdown-link>
+                            </form>
+                        </x-slot>
+                    </x-dropdown>
+                @endauth
             </div>
-
             <!-- Hamburger -->
             <div class="-me-2 flex items-center sm:hidden">
                 <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 dark:text-gray-500 hover:text-gray-500 dark:hover:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-900 focus:outline-none focus:bg-gray-100 dark:focus:bg-gray-900 focus:text-gray-500 dark:focus:text-gray-400 transition duration-150 ease-in-out">
@@ -87,36 +104,46 @@
     </div>
 
     <!-- Responsive Navigation Menu -->
-    <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
+    <div :class="{'block': open, 'hidden': ! open}" class="sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                {{ __('Dashboard') }}
+
+            <x-responsive-nav-link :href="route('show.index')" :active="request()->routeIs('show.index')">
+                {{ __('Spectacle') }}
             </x-responsive-nav-link>
-        </div>
+            @guest
+                <x-responsive-nav-link :
 
-        <!-- Responsive Settings Options -->
-        <div class="pt-4 pb-1 border-t border-gray-200 dark:border-gray-600">
-            <div class="px-4">
-                <div class="font-medium text-base text-gray-800 dark:text-gray-200">{{ Auth::user()->name }}</div>
-                <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
-            </div>
-
-            <div class="mt-3 space-y-1">
-                <x-responsive-nav-link :href="route('profile.edit')">
-                    {{ __('Profile') }}
+href="route('login')" :active="request()->routeIs('login')">
+                    {{ __('Login') }}
+                </x-responsive-nav-link>
+                @if (Route::has('register'))
+                    <x-responsive-nav-link :href="route('register')" :active="request()->routeIs('register')">
+                        {{ __('Register') }}
+                    </x-responsive-nav-link>
+                @endif
+            @endguest
+            @auth
+                <x-responsive-nav-link :href="route('artist.index')" :active="request()->routeIs('artist.index')">
+                    {{ __('Artist') }}
+                </x-responsive-nav-link>
+                <x-responsive-nav-link :href="route('type.index')" :active="request()->routeIs('type.index')">
+                    {{ __('Type') }}
+                </x-responsive-nav-link>
+                <x-responsive-nav-link :href="route('locality.index')" :active="request()->routeIs('locality.index')">
+                    {{ __('Locality') }}
+                </x-responsive-nav-link>
+                <x-responsive-nav-link :href="route('representation.index')" :active="request()->routeIs('representation.index')">
+                    {{ __('Representation') }}
+                </x-responsive-nav-link>
+                <x-responsive-nav-link :href="route('show.index')" :active="request()->routeIs('show.index')">
+                    {{ __('Spectacle') }}
+                </x-responsive-nav-link>
+                <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+                    {{ __('Dashboard') }}
                 </x-responsive-nav-link>
 
-                <!-- Authentication -->
-                <form method="POST" action="{{ route('logout') }}">
-                    @csrf
-
-                    <x-responsive-nav-link :href="route('logout')"
-                            onclick="event.preventDefault();
-                                        this.closest('form').submit();">
-                        {{ __('Log Out') }}
-                    </x-responsive-nav-link>
-                </form>
-            </div>
+            @endauth
         </div>
     </div>
 </nav>
+
