b06af930466b94f0843cbd37066c2ef272a3be5f
Hadevv <ademeure29@gmail.com>
2024-04-17 15:47:05 +0200

    Update language files for French translation

---
+++ lang/fr/pagination.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'jump_to'          => 'Aller à',
+    'jump_to_confirm'  => 'Aller à confirmer',
+    'next'             => 'Suivant &raquo;',
+    'no_results'       => 'Aucun résultat',
+    'of'               => 'de',
+    'previous'         => '&laquo; Précédent',
+    'results'          => 'résultats',
+    'results_per_page' => 'Résultats par page',
+    'showing'          => 'Affichage',
+    'to'               => 'à',
+];
