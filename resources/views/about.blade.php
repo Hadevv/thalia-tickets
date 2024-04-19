d222c241619c87ec1ae1df41df667da9dd224028
Hadevv <ademeure29@gmail.com>
2024-04-19 21:36:51 +0200

    Add about, footer, and privacy views and mention RGPD compliance

---
+++ resources/views/about.blade.php
@@ -0,0 +1,23 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('About') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-20">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="px-4 sm:px-6 lg:px-8">
+                <div class="p-4">
+                    <h2 class="font-semibold text-2xl text-gray-800 leading-tight">
+                        {{ __('It is all about nudge!') }}
+                    </h2>
+                    <div class="mt-10">
+                        <p>Discover a modern theatre ticket booking experience in Belgium</p>
+                        <p>That's why I created <a href="{{ config('app.url') }}" class="underline">reservation.com</a>,It aims to be simple and minimalist.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,16 @@
+<footer class="bg-gray-50 mt-auto">
+    <div class="mx-auto max-w-7xl text-xs px-6 py-12 md:flex md:items-center md:justify-between lg:px-8">
+        <div class="flex justify-center space-x-6 md:order-2">
+            <a href="{{ route('about') }}" class="text-gray-400 hover:text-gray-500">
+                <span>About</span>
+            </a>
+
+            <a href="{{ route('privacy') }}" class="text-gray-400 hover:text-gray-500">
+                <span>Privacy Policy</span>
+            </a>
+        </div>
+        <div class="mt-8 md:order-1 md:mt-0">
+            <p class="text-center leading-5 text-gray-500">&copy; {{ now()->format('Y') }} Reservation, Inc. All rights reserved.</p>
+        </div>
+    </div>
+</footer>
