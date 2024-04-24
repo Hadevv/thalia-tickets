b8c9d38316e7f174c9276e26881012f53875b38d
Hadevv <ademeure29@gmail.com>
2024-04-24 22:44:43 +0200

    Add laravel-debugbar package and service provider for debugging purposes

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -4,4 +4,5 @@ return [
     App\Providers\AppServiceProvider::class,
     App\Providers\AuthServiceProvider::class,
     App\Providers\EventServiceProvider::class,
+    Barryvdh\Debugbar\ServiceProvider::class,
 ];
