17895a134a44379c2b9fa5d47900a2a67f6b3624
Hadevv <ademeure29@gmail.com>
2024-04-25 18:03:10 +0200

    Add select-multiple component for handling multiple select inputs

---
+++ resources/views/components/select-multiple.blade.php
@@ -0,0 +1,11 @@
+@props(['name', 'options', 'selected' => null])
+
+<select {{ $attributes->merge(['class' => 'block mt-1 w-full border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm', 'multiple' => true]) }} name="{{ $name }}[]" multiple>
+    @foreach($options as $key => $value)
+        <option value="{{ $key }}" {{ in_array($key, (array) $selected) ? 'selected' : '' }}>{{ $value }}</option>
+    @endforeach
+</select>
+
+@error($name)
+<p class="text-sm text-red-600 mt-1" role="alert">{{ $message }}</p>
+@enderror
