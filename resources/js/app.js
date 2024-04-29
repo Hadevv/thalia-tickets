5427787fa8568fd4621f1cf63cea97ea5eb2af3a
Hadevv <ademeure29@gmail.com>
2024-04-29 15:08:22 +0200

    Remove font-awesome package

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,15 +1,11 @@
 import './bootstrap';
 
 import Alpine from 'alpinejs';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-import { faUser, faClock } from '@fortawesome/free-solid-svg-icons';
 import { createApp } from 'vue';
 import Pagination from './components/Pagination.vue';
 import Dropdown from './components/Dropdown.vue';
 import Navigation from './components/Navigation.vue';
 
-library.add(faUser, faClock);
 
 const app = createApp({
     components: {
@@ -18,8 +14,6 @@ const app = createApp({
         Navigation
     }
 });
-
-app.component('font-awesome-icon', FontAwesomeIcon);
 
 window.Alpine = Alpine;
 Alpine.start();
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,11 +10,17 @@
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-    @cookieconsentscripts
+
+    <!-- Styles -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha512-Fo3rlrZj/k7ujTnHg4CGR2D7kSs0v4LLanw2qksYuRlEzO+tcaEPQogQ0KaoGN26/zrn20ImR1DfuLWnOo7aBA==" crossorigin="anonymous" referrerpolicy="no-referrer"/>
+
     <!-- Feed rss & atom  -->
+    @cookieconsentscripts
     <x-feed-links />
+
     <!-- Scripts -->
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @vite(['resources/css/all.min.css','resources/css/app.css','resources/js/app.js'])
+
 </head>
 <body class="font-sans antialiased flex flex-col min-h-screen">
     <div class="flex-grow bg-gray-100 dark:bg-gray-900 flex flex-col">
@@ -36,9 +42,7 @@
         </main>
 
         <!-- Page Footer -->
-        @unless(request()->routeIs('home'))
         @include('layouts.footer')
-        @endunless
     </div>
     @cookieconsentview
 </body>
