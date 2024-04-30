9361e1a9be4f70e709f13a674c4e455bbb7272ba
Hadevv <ademeure29@gmail.com>
2024-04-30 15:46:00 +0200

    Update footer and navigation styles and links

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,16 +1,28 @@
 <footer class="bg-gray-50 mt-auto bg-white shadow-sm sm:rounded-lg z-1">
     <div class="mx-auto max-w-7xl text-xs px-6 py-6 md:flex md:items-center md:justify-between lg:px-8">
-        <div class="flex justify-center space-x-6 md:order-2">
+        <div class="flex justify-center space-x-6 md:order-3">
+            <!-- Contact -->
+            <a href="#" class="text-gray-400 hover:text-gray-500">
+                <span class="text-sm">Contact</span>
+            </a>
+
+            <!-- A propos -->
             <a href="{{ route('about') }}" class="text-gray-400 hover:text-gray-500">
                 <span class="text-sm">About</span>
             </a>
 
+            <!-- Politique de confidentialité -->
             <a href="{{ route('privacy') }}" class="text-gray-400 hover:text-gray-500">
                 <span class="text-sm">Privacy Policy</span>
             </a>
         </div>
         <div class="mt-8 md:order-1 md:mt-0">
             <p class="text-center leading-5 text-sm text-gray-500">&copy; {{ now()->format('Y') }} Reservation, Inc. All rights reserved.</p>
+        </div>
+        <div class="mt-8 md:order-2 md:mt-0">
+            <a href="#" class="text-gray-400 hover:text-gray-500">
+                <span class="text-sm">Billetterie | +32 2 505 30 30 | <a class="text-sm text-gray-400 hover:text-gray-500" href="mailto:billetterie@reservations.be">billetterie@reservations.be</a> </span>
+            </a>
         </div>
     </div>
 </footer>
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -11,7 +11,7 @@
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('home') }}">
-                        <img src="{{ asset('images/theater.png') }}" class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
+                        <img src="{{ asset('images/theater.png') }}" class="block h-9 w-auto text-gray-800 dark:text-gray-200" />
                     </a>
                 </div>
 
@@ -23,6 +23,9 @@
                     </x-nav-link>
                     <x-nav-link :href="route('show.index')" :active="request()->routeIs('show.index')">
                         {{ __('Spectacle') }}
+                    </x-nav-link>
+                    <x-nav-link :href="route('schedule.index')" :active="request()->routeIs('schedule.index')">
+                        {{ __('Horaire') }}
                     </x-nav-link>
                     {{-- Guest Links --}}
                     {{-- Auth Links --}}
