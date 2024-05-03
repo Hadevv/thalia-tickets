01457d4e414eccd4dbad464b7a633e44a5e13721
Hadevv <ademeure29@gmail.com>
2024-05-03 23:18:54 +0200

    Fix RepresentationObserver logic to trigger RepresentationEnded event when schedule is updated and in the past

--- app/Observers/RepresentationObserver.php
+++ app/Observers/RepresentationObserver.php
@@ -4,33 +4,33 @@ namespace App\Observers;
 
 use App\Models\Representation;
 use App\Models\RepresentationArchive;
+use App\Events\RepresentationEnded;
 
 class RepresentationObserver
 {
+    /**
+     * Vérifie si la date de la representation est passée et si c'est le cas, envoie un event pour la fin de la representation
+     */
+    public function updated(Representation $representation): void
+    {
+        if ($representation->isDirty('schedule') && $representation->schedule < now()) {
+            event(new RepresentationEnded($representation));
+        }
+    }
+    /**
+     * Suppression d'une representation dans la table representation et l'ajout dans la table representation_archive
+     */
+    public function deleted(Representation $representation): void
+    {
+        RepresentationArchive::create($representation->toArray());
+    }
+
     /**
      * Handle the Representation "created" event.
      */
     public function created(Representation $representation): void
     {
-
-
-    }
-
-    /**
-     * Handle the Representation "updated" event.
-     */
-    public function updated(Representation $representation): void
-    {
         //
-    }
-
-    /**
-     * Handle the Representation "deleted" event.
-     */
-    public function deleted(Representation $representation): void
-    {
-        RepresentationArchive::create($representation->toArray());
-
     }
 
     /**
