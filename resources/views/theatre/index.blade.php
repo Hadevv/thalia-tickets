a7166b6e1d7d8100681134769d9a3daa4c38d1e4
Hadevv <ademeure29@gmail.com>
2024-05-08 10:49:08 +0200

    chore: Update theatre index page layout and styles

---
+++ resources/views/theatre/index.blade.php
@@ -0,0 +1,91 @@
+<x-app-layout>
+    <div class="py-6">
+        <div class=" mx-auto sm:px-6 lg:px-6">
+            <div class="bg-white dark:bg-gray-800 md:mr-20 md:ml-20 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100 relative">
+                    <div class="grid gap-3 grid-cols-12 grid-rows-1">
+                        <div
+                            class="col-span-full grid-row-2 sm:col-span-6 lg:col-span-3 overflow-hidden relative bg-white dark:bg-gray-900">
+                            <!-- Card -->
+                            <div class="p-6 bg-[url('/public/images/)]
+                                h-96
+                                bg-cover
+                                bg-center
+                                bg-no-repeat">
+                            </div>
+                            <div class="mt-3 p-6 bg-[url('/public/images/)]
+                                h-96
+                                bg-cover
+                                bg-center
+                                bg-no-repeat">
+                            </div>
+                        </div>
+                        <div
+                            class="flex justify-between col-span-full sm:col-span-6 lg:col-span-3 overflow-hidden relative dark:bg-gray-900">
+                            <!-- Card Repertoire -->
+                            <div class="
+                                    text-white-900
+                                    dark:text-white-100
+                                    bg-[url('/public/images/)]
+                                    h-full
+                                    w-full
+                                    bg-right
+                                    bg-cover
+                                    bg-no-repeat
+                                    bg-opacity-30
+                                    filter-grayscale
+                                    hover:brightness-125
+                                    transition
+                                    duration-300
+                                    ease-in-out
+                                    transform
+                                    hover:scale-105
+                                    bg-top">
+                                <div class="h-full p-6">
+
+                                </div>
+                                <div class="m-2 mt:4 md:mt-8 flex justify-between items-center z-100 h-auto">
+                                    <div class="
+                                            flex
+                                            justify-between
+                                            items-center
+                                            opacity-70
+                                            w-full
+                                            font-semibold
+                                            text-base
+                                            text-white
+                                            dark:text-white-300
+                                            bg-red-700
+                                            hover:bg-red-800
+                                            p-2
+                                            rounded
+                                            focus:outline-none
+                                            focus:shadow-outline">
+                                        <!-- repertoire -->
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div
+                            class="col-span-full text-withe sm:col-span-6 lg:col-span-6 overflow-hidden relative bg-white dark:bg-gray-900">
+                            <!-- Card -->
+                            <div class="
+                                    p-6
+                                    text-white dark:text-white-300
+                                    bg-[url('/public/images/)]
+                                    h-full
+                                    bg-cover
+                                    bg-no-repeat
+                                    bg-opacity-30
+                                    bg-top
+                                    ">
+                                <div class="flex justify-end items-center h-full">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
