7dbd586fbd8c48458c66e10393d63ac5580ebfff
Hadevv <ademeure29@gmail.com>
2024-05-18 14:40:06 +0200

    deploy: Update Vite configuration for lazy loading images and change server port to 5173

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,17 @@ import laravel from "laravel-vite-plugin";
 import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
+        server: {
+            host: '0.0.0.0',
+            port: 5173,
+            hmr: {
+                host: 'localhost',
+            },
+            watch: {
+                usePolling: true,
+            },
+        },
+    
     plugins: [
         laravel({
             input: ["resources/css/app.css", "resources/js/app.js"],
@@ -23,3 +34,5 @@ export default defineConfig({
         },
     },
 });
+
+
