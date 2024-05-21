e2e63bfa4bbb800ce73357b845a1699465f90cd3
Hadevv <ademeure29@gmail.com>
2024-05-21 18:16:02 +0200

    chore: Update npm dependencies and add 'preline' library for code formatting

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,41 +1,39 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
-import forms from '@tailwindcss/forms';
+import defaultTheme from "tailwindcss/defaultTheme";
+import forms from "@tailwindcss/forms";
 
 /** @type {import('tailwindcss').Config} */
 export default {
     // changer le theme en blanc
-    darkMode: 'class',
+    darkMode: "class",
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/views/**/*.blade.php",
+        "node_modules/preline/dist/*.js",
     ],
 
     theme: {
         extend: {
             colors: {
-                'primary': '#5a67d8',
-                'secondary': '#5a67d8',
-                'danger': '#FF0000',
-                'success': '#008000',
-                'info': '#00FFFF',
-                'warning': '#FFA500',
-                'dark': '#000000',
-                'brezee' :'#5a67d8',
+                primary: "#5a67d8",
+                secondary: "#5a67d8",
+                danger: "#FF0000",
+                success: "#008000",
+                info: "#00FFFF",
+                warning: "#FFA500",
+                dark: "#000000",
+                brezee: "#5a67d8",
             },
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                sans: ["Figtree", ...defaultTheme.fontFamily.sans],
 
-                macamore: ['Macamore', 'sans-serif'],
-
+                macamore: ["Macamore", "sans-serif"],
             },
             backgroundImage: {
-                'hero-repertoire': "url('public/images/repertoire.jpg')",
-            }
+                "hero-repertoire": "url('public/images/repertoire.jpg')",
+            },
         },
     },
 
-    plugins: [forms],
-
-
+    plugins: [require("@tailwindcss/forms"), require("preline/plugin")],
 };
