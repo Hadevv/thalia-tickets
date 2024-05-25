bb8e01a7ce914290245023ee1ef72efb5a8a2267
Hadevv <ademeure29@gmail.com>
2024-05-25 23:12:35 +0200

    feat: Add TagPolicy for authorization control and update AuthServiceProvider
    - Add TagPolicy for authorization control
    - Update AuthServiceProvider to include TagPolicy in the $policies array

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
+use App\Models\Tag;
+use App\Policies\TagPolicy;
 use App\Models\User;
 use App\Policies\AdminPolicy;
 
@@ -11,6 +13,7 @@ class AuthServiceProvider extends ServiceProvider
 {
     protected $policies = [
         User::class => AdminPolicy::class,
+        Tag::class => TagPolicy::class,
     ];
 
     public function boot()
