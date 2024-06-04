da5f6f0b4b8ff1a9e8b3aeed2e54225028f9f903
Hadevv <ademeure29@gmail.com>
2024-06-04 15:40:16 +0200

    refactor: Update search bar and keyword search components
    - Update the search bar and keyword search components to use the 'w-full' class for full-width display.
    - This change ensures that the search bar and keyword search components take up the full width of their parent container.

--- resources/views/components/keyword-search.blade.php
+++ resources/views/components/keyword-search.blade.php
@@ -1,7 +1,7 @@
 @props(['search'])
 
 <form action="{{ route('show.index') }}" method="GET" id="searchForm" class="flex">
-    <div class="relative w-[35ch] max-w-[40ch]">
+    <div class="relative w-full w-[35ch] max-w-[40ch]">
         <input type="text" name="search" value="{{ $search }}" placeholder="Keyword..." class="w-full border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm pr-8" id="searchInput">
             <a href="{{ route('show.clear') }}" class="absolute inset-y-0 right-0 mr-3 flex items-center justify-center text-gray-400 hover:text-gray-500 focus:outline-none">
                 <svg class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
--- resources/views/show/index.blade.php
+++ resources/views/show/index.blade.php
@@ -2,35 +2,42 @@
     <div class="flex justify-center flex-col py-6 px-4 md:flex-row md:px-0 md:mr-24 md:ml-24 md:py-6">
         <div class="w-full md:w-1/3 bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg mx-2 md:mx-1 mb-4">
             <div class="sm:min-w-36 p-6 text-gray-900 dark:text-gray-100">
-                <!-- Contenu de la première zone blanche -->
-                <x-search-bar :shows="$shows" :search="$search ?? '' "/>
-                <x-input-label for="recherche" value="Recherche par mot clé" class="
-                text
-                font-semibold
-                text-lg
-                text-gray
-                dark:text-gray-200
-            "/>
-                <x-keyword-search :shows="$shows" :search="$search ?? '' "/>
-                <h3 class="
-                    text
-                    font-semibold
-                    text-lg
-                    text-gray
-                    dark:text-gray-200
-                ">Filtrer les spectacles</h3>
+                <h3 class="text font-semibold text-lg text-gray dark:text-gray-200">
+                    Rechercher un spectacle
+                </h3>
+
+                <!-- barre de recherche de spectacles -->
+                <x-input-label class="text font-semibold text-lg text-gray dark:text-gray-200">
+                    Rechercher un spectacle
+                </x-input-label>
+                <x-search-bar :shows="$shows" :search="$search ?? ''" />
+
+                <!-- recherche par mot clé -->
+                <x-input-label for="recherche" value="Recherche par mot clé" class="text font-semibold text-lg text-gray dark:text-gray-200" />
+                <x-keyword-search :shows="$shows" :search="$search ?? ''" />
+
+
+                <h3 class="text font-semibold text-lg text-gray dark:text-gray-200">
+                    Filtrer les spectacles
+                </h3>
+
+                <!-- formulaire -->
                 <form action="{{ route('show.index') }}" method="get" id="filterForm">
+                    <!-- filtre par date de début -->
                     <x-input-label for="date_from" value="De" />
-                    <input type="date" name="date_from" id="date_from" value="{{ $date_from ?? '' }}" onchange="submitForm()" class="w-full w-[35ch] max-w-[40ch] border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm">
+                    <input type="date" name="date_from" id="date_from" value="{{ $date_from ?? '' }}" onchange="submitForm()" class="w-full max-w-[40ch] border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm" />
 
+                    <!-- filtre par date de fin -->
                     <x-input-label for="date_to" value="à" />
-                    <input type="date" name="date_to" id="date_to" value="{{ $date_to ?? '' }}" onchange="submitForm()" class="w-full w-[35ch] max-w-[40ch] border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm">
+                    <input type="date" name="date_to" id="date_to" value="{{ $date_to ?? '' }}" onchange="submitForm()" class="w-full max-w-[40ch] border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm" />
 
+                    <!-- filtre par lieu -->
                     <x-input-label for="location" value="Lieux" />
-                    <x-select name="location" :options="$lieux ?? []" :selected="$location ?? null" onchange="submitForm()" class="w-[35ch] max-w-[40ch]" />
+                    <x-select name="location" :options="$lieux ?? []" :selected="$location ?? null" onchange="submitForm()" class="w-full max-w-[40ch]" />
                 </form>
             </div>
         </div>
+
         <div class="w-full md:w-2/3 bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg mx-2 md:mx-1 md:sm">
             <div class="p-6 text-gray-900 dark:text-gray-100 overflow-y-auto max-h-[700px] md:sm w-full">
                 <!-- Contenu de la deuxième zone blanche -->
