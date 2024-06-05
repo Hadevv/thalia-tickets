d0935aa04b589bf6ad652c1e9b1dc784c36a0c8b
Hadevv <ademeure29@gmail.com>
2024-06-05 14:29:18 +0200

    refactor: Update search bar and keyword search components

--- resources/views/show/index.blade.php
+++ resources/views/show/index.blade.php
@@ -13,9 +13,9 @@
                 <x-search-bar :shows="$shows" :search="$search ?? ''" />
 
                 <!-- recherche par mot clé -->
-                <x-input-label for="recherche" value="Recherche par mot clé" class="text font-semibold text-lg text-gray dark:text-gray-200" />
+                <x-input-label for="recherche" value="Recherche par mot clé"
+                    class="text font-semibold text-lg text-gray dark:text-gray-200" />
                 <x-keyword-search :shows="$shows" :search="$search ?? ''" />
-
 
                 <h3 class="text font-semibold text-lg text-gray dark:text-gray-200">
                     Filtrer les spectacles
@@ -25,20 +25,26 @@
                 <form action="{{ route('show.index') }}" method="get" id="filterForm">
                     <!-- filtre par date de début -->
                     <x-input-label for="date_from" value="De" />
-                    <input type="date" name="date_from" id="date_from" value="{{ $date_from ?? '' }}" onchange="submitForm()" class="w-full max-w-[40ch] border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm" />
+                    <input type="date" name="date_from" id="date_from" value="{{ $date_from ?? '' }}"
+                        onchange="submitForm()"
+                        class="w-full max-w-[40ch] border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm" />
 
                     <!-- filtre par date de fin -->
                     <x-input-label for="date_to" value="à" />
-                    <input type="date" name="date_to" id="date_to" value="{{ $date_to ?? '' }}" onchange="submitForm()" class="w-full max-w-[40ch] border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm" />
+                    <input type="date" name="date_to" id="date_to" value="{{ $date_to ?? '' }}"
+                        onchange="submitForm()"
+                        class="w-full max-w-[40ch] border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm" />
 
                     <!-- filtre par lieu -->
                     <x-input-label for="location" value="Lieux" />
-                    <x-select name="location" :options="$lieux ?? []" :selected="$location ?? null" onchange="submitForm()" class="w-full max-w-[40ch]" />
+                    <x-select name="location" :options="$lieux ?? []" :selected="$location ?? null" onchange="submitForm()"
+                        class="w-full max-w-[40ch]" />
                 </form>
             </div>
         </div>
 
-        <div class="w-full md:w-2/3 bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg mx-2 md:mx-1 md:sm">
+        <div
+            class="w-full md:w-2/3 bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg mx-2 md:mx-1 md:sm">
             <div class="p-6 text-gray-900 dark:text-gray-100 overflow-y-auto max-h-[700px] md:sm w-full">
                 <!-- Contenu de la deuxième zone blanche -->
                 <!-- les spectacles -->
@@ -53,24 +59,31 @@
     </div>
 
     @push('scripts')
-        <script>
-            function submitForm() {
-                const dateFrom = document.getElementById('date_from').value;
-                const dateTo = document.getElementById('date_to').value;
+    <script>
+        function submitForm() {
+            const dateFrom = document.getElementById('date_from').value;
+            const dateTo = document.getElementById('date_to').value;
+            const location = document.querySelector('select[name="location"]').value;
 
-                // Vérifier si les deux dates sont définies
-                if (dateFrom && dateTo) {
-                    // Si les deux dates sont définies, soumettre le formulaire
-                    console.log("Form submitted");
-                    document.getElementById('filterForm').submit();
-                } else {
-                    alert("Veuillez sélectionner à la fois une date de début et une date de fin.");
-                }
+            console.log("Date de début:", dateFrom);
+            console.log("Date de fin:", dateTo);
+            console.log("Lieu:", location);
+
+            // soumettre le formulaire si les deux dates sont définies ou si la localisation est définie
+            if ((dateFrom && dateTo) || location) {
+                console.log("Form submitted");
+                document.getElementById('filterForm').submit();
+            } else {
+                alert("Veuillez sélectionner à la fois une date de début et une date de fin ou un lieu.");
             }
-        </script>
+        }
+
+        // attachez les événements pour les champs du formulaire
+        document.getElementById('date_from').addEventListener('change', submitForm);
+        document.getElementById('date_to').addEventListener('change', submitForm);
+        document.querySelector('select[name="location"]').addEventListener('change', submitForm);
+    </script>
     @endpush
+
 </x-app-layout>
-
-
-
 
